f8458d3555348c222579716949c380668797de1a
samBoyle <sboyle05@gmail.com>
2023-11-28 15:31:07 -0800

    tailwind css added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-
+import LandingPage from './components/landingPage/landingPage';
+import About from './components/about/about';
+import Contact from './components/contact/contact';
+import Navbar from './components/navbar/navbar'
+import Projects from './components/projects/projects';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </Router>
   );
 }
 
