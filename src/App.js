9cbfee296416d3a9acda4c9ac64d364bf86647cd
samBoyle <sboyle05@gmail.com>
2023-11-28 15:42:34 -0800

    link removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/landingPage/landingPage';
 import About from './components/about/about';
